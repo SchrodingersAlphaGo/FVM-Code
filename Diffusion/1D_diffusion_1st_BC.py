e4044eb93d1789fb40aeeaff8ab2ba2598ae4bc6
SchrodingersAlphaGo <anyong_zhang@163.com>
2023-07-22 18:36:30 +0800

    refactor (case: 1d diffusion 1st bc)

--- Diffusion/1D_diffusion_1st_BC.py
+++ Diffusion/1D_diffusion_1st_BC.py
@@ -1,20 +1,14 @@
-'''
-有限体积法求解一维扩散问题（第一类边界条件）
-离散方程推导见博文：
-https://blog.csdn.net/weixin_42562856/article/details/106799542
-'''
-
 import numpy as np 
 import matplotlib.pyplot as plt
 
 #== parameters ===
-nx = 5  # 网格单元数
-nndoes = nx + 2 # 节点数，含边界
+num_elements = 5  # 网格单元数
+num_nodes = num_elements + 2 # 节点数，含边界
 
 #===  for example 1 ==========
 ''' 
-L = 0.5 # 长度，m
-Area = 0.01 #截面面积 ,m2
+length = 0.5 # 长度，m
+area = 0.01 #截面面积 ,m2
 k = 1000  #导热系数 , W/m-k
 Ta = 100 # 边界A的温度值 ,C
 Tb = 500 # 边界B的温度值 ,C
@@ -24,8 +18,8 @@ q = 0 # kW/m3, 体积热源
 
 #***** for example 2 *********
 #'''
-L = 0.02 # 长度，m
-Area = 1 #截面面积 ,m2
+length = 0.02 # 长度，m
+area = 1 #截面面积 ,m2
 k = 0.5  #导热系数 , W/m-k
 Ta = 100 # 边界A的温度值 ,C
 Tb = 200 # 边界B的温度值 ,C
@@ -35,42 +29,43 @@ q = 1e6 # W/m3, 体积热源
 
 
 #==  x grid ==
-dx = L/nx  # 网格间距
+dx = length/num_elements  # 网格间距
 print('dx = ',dx)
-x = np.zeros(nndoes)  # x网格
-x[1:nndoes-1] = np.linspace(dx/2, L-dx/2, nx) # 以边界A为原点创建网格点的坐标值
+x = np.zeros(num_nodes)  # x网格
+x[1:num_nodes-1] = np.linspace(dx/2, length-dx/2, num_elements) # 以边界A为原点创建网格点的坐标值
 x[-1] = x[-2] + dx/2 #边界B的坐标值
 print('x grid = ', x, '\n')
 
 #==  solution array ==
-tt = np.zeros(nndoes)  # 解向量
+tt = np.zeros(num_nodes)  # 解向量, i.e. 温度向量
 tt[0] = Ta # 边界值
 tt[-1] = Tb
 
 #== matrix ==
-A = np.zeros((nx, nx)) 
-b = np.zeros(nx)
+A = np.zeros((num_elements, num_elements)) 
+b = np.zeros(num_elements)
 
+# source term
 su = q*dx
-for i in range(1, nx-1):    # 内部网格节点
-    A[i][i-1] = -k*Area/dx
-    A[i][i+1] = -k*Area/dx
+for i in range(1, num_elements-1):    # 内部网格节点
+    A[i][i-1] = -k*area/dx
+    A[i][i+1] = -k*area/dx
     A[i][i] = -(A[i][i-1] + A[i][i+1])
     b[i] = su
 
 # for boundary A
 i = 0  
-A[i][i+1] = -k*Area/dx
-su = 2*k*Area*Ta/dx + q*dx
-sp = -2*k*Area/dx
+A[i][i+1] = -k*area/dx
+su = 2*k*area*Ta/dx + q*dx
+sp = -2*k*area/dx
 A[i][i] = -A[i][i+1] - sp
 b[i] = su
 
 # for boundary B
-i = nx-1  
-A[i][i-1] = -k*Area/dx
-su = 2*k*Area*Tb/dx + q*dx
-sp = -2*k*Area/dx
+i = num_elements-1  
+A[i][i-1] = -k*area/dx
+su = 2*k*area*Tb/dx + q*dx
+sp = -2*k*area/dx
 A[i][i] = -A[i][i-1] - sp
 b[i] = su
 
@@ -79,13 +74,13 @@ print('b = \n', np.matrix(b).T ,'\n')
 
 t_temp = np.linalg.solve(A, b)
 print('solution = \n', np.matrix(t_temp).T, '\n')
-tt[1:nndoes-1] = t_temp
+tt[1:num_nodes-1] = t_temp
 
-xx = np.linspace(0, L, 50, endpoint=True)
+xx = np.linspace(0, length, 50, endpoint=True)
 exact_tt = np.zeros(50)
 for i in range(50):
     #exact_tt[i] = 800*xx[i] + 100  # 例1 解析解
-    exact_tt[i] = ((Tb-Ta)/L + q*(L-xx[i])/(2*k)) * xx[i] + Ta # 例2 解析解
+    exact_tt[i] = ((Tb-Ta)/length + q*(length-xx[i])/(2*k)) * xx[i] + Ta # 例2 解析解
     
 plt.xlabel('Distance (cm)')
 plt.ylabel('Temperature (C)')
